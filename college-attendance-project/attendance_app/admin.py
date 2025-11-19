9210f74d3e8b5b7e1418bb881f091bcb0b871d08
prriyanshi2012006-droid <prriyanshi2012006@gmail.com>
2025-11-19 18:55:27 +0530

    commit

--- college-attendance-project/attendance_app/admin.py
+++ college-attendance-project/attendance_app/admin.py
@@ -1,3 +1,17 @@
+# attendance_app/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, StudentProfile, Course, AttendanceRecord
 
-# Register your models here.
+# Customize the display of the User model in the Admin panel
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'role', 'department')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role', 'department', 'phone_number')}),
+    )
+
+# Register all models
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(StudentProfile)
+admin.site.register(Course)
+admin.site.register(AttendanceRecord)
--- college-attendance-project/core_backend/urls.py
+++ college-attendance-project/core_backend/urls.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import RedirectView # <-- Import this
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,4 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('attendance_app.urls')),
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
 ]
